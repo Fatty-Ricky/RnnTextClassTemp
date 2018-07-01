01e068b7e600b6646033046cc07d0dda8aedd60d
Rick-YSN <312390695@qq.com>
2018-07-01 18:15:10 +0800

    Add files via upload

--- text_classification.py
+++ text_classification.py
@@ -19,24 +19,71 @@ from __future__ import print_function
 
 import argparse
 import sys
+import os
+import tarfile
 
-import numpy as np
-import pandas
-from sklearn import metrics
+from tensorflow.contrib.learn import Experiment
+import pandas as pd
 import tensorflow as tf
+import tensorlayer as tl
+
+from tensorflow.python import debug as tf_debug
+
+hooks = [tf_debug.LocalCLIDebugHook()]
 
 FLAGS = None
 
 MAX_DOCUMENT_LENGTH = 10
 EMBEDDING_SIZE = 50
-n_words = 0
-MAX_LABEL = 15
-WORDS_FEATURE = 'words'  # Name of the input words feature.
+MAX_LABEL=15
+
+DATA_DIR = 'data'
+DBPEDIA_URL = 'https://github.com/le-scientifique/torchDatasets/raw/master/dbpedia_csv.tar.gz'
+VOCAB_PATH = ''
+BATCH_SIZE = 10
+CVS_COLUMN_NAME = ['Category','Name','Text']
+
+def load_dataset(file_path, vocab):
+    csv = pd.read_csv(file_path, names=CVS_COLUMN_NAME, header=0)
+    data = csv.pop('Text').values
+    category = csv.pop('Category').values
+
+    text_embedings = []
+    for text in data:
+        desc = tl.nlp.process_sentence(text, start_word=None, end_word=None,  )
+        embedding = tl.nlp.words_to_word_ids(desc, word_to_id= vocab)
+        text_embedings.append(embedding)
+
+    text_embedings = tl.prepro.pad_sequences(text_embedings, maxlen=MAX_DOCUMENT_LENGTH)
+    category = tf.one_hot(category,depth= MAX_LABEL)
+    dataset = tf.data.Dataset.from_tensor_slices(({"Text":text_embedings}, category))
+    if FLAGS.small:
+        dataset = dataset.shuffle(1000)
+
+    return dataset
+
+
+def train_input_fn(vocab):
+    train_path = os.path.join(sys.path[0], DATA_DIR, 'dbpedia_csv', 'train.csv')
+
+    train_dataset = load_dataset(train_path, vocab)
+    train_dataset = train_dataset.repeat(3)
+    train_dataset = train_dataset.batch(BATCH_SIZE)
+
+    return train_dataset.make_one_shot_iterator().get_next()
+
+
+def test_input_fn(vocab):
+    test_path = os.path.join(sys.path[0], DATA_DIR, 'dbpedia_csv' ,'test.csv')
+    test_dataset = load_dataset(test_path, vocab)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+    return test_dataset.make_one_shot_iterator().get_next()
 
 
 def estimator_spec_for_softmax_classification(logits, labels, mode):
   """Returns EstimatorSpec instance for softmax classification."""
   predicted_classes = tf.argmax(logits, 1)
+  predicted_onehot = tf.one_hot(predicted_classes, MAX_LABEL)
   if mode == tf.estimator.ModeKeys.PREDICT:
     return tf.estimator.EstimatorSpec(
         mode=mode,
@@ -45,15 +92,16 @@ def estimator_spec_for_softmax_classification(logits, labels, mode):
             'prob': tf.nn.softmax(logits)
         })
 
-  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+  loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits)
+  accuracy = tf.metrics.accuracy(labels=labels, predictions=predicted_onehot)
+  tf.summary.scalar('Accuracy', accuracy[1])
   if mode == tf.estimator.ModeKeys.TRAIN:
     optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
   eval_metric_ops = {
-      'accuracy':
-          tf.metrics.accuracy(labels=labels, predictions=predicted_classes)
+      'accuracy':accuracy
   }
   return tf.estimator.EstimatorSpec(
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
@@ -62,7 +110,7 @@ def estimator_spec_for_softmax_classification(logits, labels, mode):
 def bag_of_words_model(features, labels, mode):
   """A bag-of-words model. Note it disregards the word order in the text."""
   bow_column = tf.feature_column.categorical_column_with_identity(
-      WORDS_FEATURE, num_buckets=n_words)
+      key='Text', num_buckets=n_words)
   bow_embedding_column = tf.feature_column.embedding_column(
       bow_column, dimension=EMBEDDING_SIZE)
   bow = tf.feature_column.input_layer(
@@ -80,7 +128,7 @@ def rnn_model(features, labels, mode):
   # maps word indexes of the sequence into [batch_size, sequence_length,
   # EMBEDDING_SIZE].
   word_vectors = tf.contrib.layers.embed_sequence(
-      features[WORDS_FEATURE], vocab_size=n_words, embed_dim=EMBEDDING_SIZE)
+      features['Text'], vocab_size=n_words, embed_dim=EMBEDDING_SIZE)
 
   # Split into list of embedding per word, while removing doc length dim.
   # word_list results to be a list of tensors [batch_size, EMBEDDING_SIZE].
@@ -101,80 +149,76 @@ def rnn_model(features, labels, mode):
       logits=logits, labels=labels, mode=mode)
 
 
-def main(unused_argv):
-  global n_words
-  tf.logging.set_verbosity(tf.logging.INFO)
+def main(_):
+    global n_words
+    tf.logging.set_verbosity(tf.logging.INFO)
 
-  # Prepare training and testing data
-  dbpedia = tf.contrib.learn.datasets.load_dataset(
-      'dbpedia', test_with_fake_data=FLAGS.test_with_fake_data)
-  x_train = pandas.Series(dbpedia.train.data[:, 1])
-  y_train = pandas.Series(dbpedia.train.target)
-  x_test = pandas.Series(dbpedia.test.data[:, 1])
-  y_test = pandas.Series(dbpedia.test.target)
+    if not tf.gfile.Exists(sys.path[0] +'/'+ DATA_DIR):
+        tf.gfile.MakeDirs(sys.path[0] +'/'+ DATA_DIR)
+        fname = os.path.join(sys.path[0], DATA_DIR, DBPEDIA_URL.split('/')[-1])
+        tf.keras.utils.get_file(fname, DBPEDIA_URL, extract = True)
+        zipped_file = tarfile.open(fname,'r:*')
+        zipped_file.extractall(sys.path[0] +'/'+ DATA_DIR)
 
-  # Process vocabulary
-  vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(
-      MAX_DOCUMENT_LENGTH)
+        print('Successfully downloaded', fname)
 
-  x_transform_train = vocab_processor.fit_transform(x_train)
-  x_transform_test = vocab_processor.transform(x_test)
 
-  x_train = np.array(list(x_transform_train))
-  x_test = np.array(list(x_transform_test))
+    train_path = os.path.join(sys.path[0] ,DATA_DIR, 'dbpedia_csv', 'train.csv')
+    vocab = tl.nlp.build_vocab(tl.nlp.read_words(train_path))
+    n_words = len(vocab)
+    vocab['UNK'] = n_words
+    n_words += 1
 
-  n_words = len(vocab_processor.vocabulary_)
-  print('Total words: %d' % n_words)
 
-  # Build model
-  # Switch between rnn_model and bag_of_words_model to test different models.
-  model_fn = rnn_model
-  if FLAGS.bow_model:
-    # Subtract 1 because VocabularyProcessor outputs a word-id matrix where word
-    # ids start from 1 and 0 means 'no word'. But
-    # categorical_column_with_identity assumes 0-based count and uses -1 for
-    # missing word.
-    x_train -= 1
-    x_test -= 1
-    model_fn = bag_of_words_model
-  classifier = tf.estimator.Estimator(model_fn=model_fn)
+    # Build model
+    # Switch between rnn_model and bag_of_words_model to test different models.
+    model_fn = rnn_model
+    if FLAGS.bow_model:
+        # Subtract 1 because VocabularyProcessor outputs a word-id matrix where word
+        # ids start from 1 and 0 means 'no word'. But
+        # categorical_column_with_identity assumes 0-based count and uses -1 for
+        # missing word.
+        model_fn = bag_of_words_model
+    classifier = tf.estimator.Estimator(model_fn=model_fn, model_dir='ckpt/')
 
-  # Train.
-  train_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={WORDS_FEATURE: x_train},
-      y=y_train,
-      batch_size=len(x_train),
-      num_epochs=None,
-      shuffle=True)
-  classifier.train(input_fn=train_input_fn, steps=100)
+    # Train.
+    classifier.train(input_fn=lambda:train_input_fn(vocab), steps=1000)
 
-  # Predict.
-  test_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={WORDS_FEATURE: x_test}, y=y_test, num_epochs=1, shuffle=False)
-  predictions = classifier.predict(input_fn=test_input_fn)
-  y_predicted = np.array(list(p['class'] for p in predictions))
-  y_predicted = y_predicted.reshape(np.array(y_test).shape)
+    # Score with tensorflow.
+    scores = classifier.evaluate(input_fn=lambda: test_input_fn(vocab))
 
-  # Score with sklearn.
-  score = metrics.accuracy_score(y_test, y_predicted)
-  print('Accuracy (sklearn): {0:f}'.format(score))
+    #debug
+    '''
+    
+    ex = Experiment(classifier,
+                    train_input_fn=lambda:train_input_fn(vocab),
+                    eval_input_fn=lambda: test_input_fn(vocab),
+                    train_steps=1000,
+                    eval_delay_secs=0,
+                    eval_steps=1,
+                    train_monitors=hooks,
+                    eval_hooks=hooks)
 
-  # Score with tensorflow.
-  scores = classifier.evaluate(input_fn=test_input_fn)
-  print('Accuracy (tensorflow): {0:f}'.format(scores['accuracy']))
+    ex.train()
+    scores = ex.evaluate()
+    '''
+
+    print('Accuracy: {0:f}'.format(scores['accuracy']))
+
+    # Predict.
+    #predictions = classifier.predict(input_fn=lambda: test_input_fn(vocab))
+    #y_predicted = np.array(list(p['class'] for p in predictions))
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--test_with_fake_data',
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+      '--small',
       default=False,
-      help='Test the example code with fake data.',
-      action='store_true')
-  parser.add_argument(
+      help='load 1000 record only.')
+    parser.add_argument(
       '--bow_model',
-      default=False,
-      help='Run with BOW model instead of RNN.',
-      action='store_true')
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+      default=True,
+      help='Run with BOW model instead of RNN.')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
